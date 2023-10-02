0dc7a95843801d3c755b6a34ebdb8df5ab6092ad
Jerome <fabricantejerome@gmail.com>
2023-10-02 14:34:27 +0800

    Create login form

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   
   # root "articles#index"
+
+  get "login", to: "sessions#new"
 
   get "blank", to: "welcome#blank"
 
