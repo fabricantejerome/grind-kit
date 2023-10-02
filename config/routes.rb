4877e12f772706c5a5171736c39d2390852aa0f6
Jerome <fabricantejerome@gmail.com>
2023-10-02 14:11:17 +0800

    Replace all DOMContentLoaded to turbo:load

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  
   # root "articles#index"
+
+  get "blank", to: "welcome#blank"
 
   root to: "welcome#index"
 end
