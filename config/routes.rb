9d247760a1ac3f304a129af92c677eed67ecf6c6
Jerome <fabricantejerome@gmail.com>
2023-10-04 09:59:35 +0800

    Add user authentication, validation, and set the current user

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
   post "sign_up", to: "registrations#create"
 
   get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+
+  delete "logout", to: "sessions#destroy"
 
   get "blank", to: "welcome#blank"
 
