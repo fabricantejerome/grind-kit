5fe52c518309246a7350d69f1b6b54a765605085
Jerome <fabricantejerome@gmail.com>
2023-10-30 12:59:57 +0800

    Add POS page interactivity

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,64 +1,88 @@
 <script setup>
 import { onBeforeMount, ref, watch } from 'vue'
 
-let items = ref([]);
-let cart = ref([]);
-let selected = ref();
-let totalAmount = ref(0);
+const items = ref([]);
+const cart = ref([]);
+const selected = ref();
+const totalAmount = ref(0);
+const cash = ref(0);
+const cantTransact = ref(true)
 
 onBeforeMount(async () => {
-  const response = await fetch(`items/json`);
-  const results = await response.json();
-  items.value = results;
+    const response = await fetch(`items/json`);
+    const results = await response.json();
+    items.value = results;
 })
 
+const updateRowTotal = (index) => {
+    const currentRow = cart.value[index]
+
+    cart.value[index].total = currentRow.price * currentRow.quantity
+
+    totalAmount.value = cart.value.reduce((acc, c) =>  acc + c.total, 0);
+}
+
+const clearAll = () => {
+    if (cart.value.length > 0) {
+        cart.value = [];
+        selected.value = null;
+        totalAmount.value = 0;
+        cash.value = 0;
+        cantTransact.value = true
+    }
+}
+
+const removeFromCart = (index) => {
+    cart.value.splice(index, 1)
+
+    totalAmount.value = cart.value.reduce((acc, c) =>  acc + c.total, 0);
+}
 
 watch(selected, (newSelected) => {
     const index = cart.value.findIndex(c => c.id === newSelected.id);
 
-    if (index == -1) {
-        const item = {
-            id: newSelected.id,
-            name: newSelected.name,
-            price: newSelected.price,
-            quantity: 1,
-            total: newSelected.price
-        }
+    if (newSelected) {
+        if (index == -1) {
+            const item = {
+                id: newSelected.id,
+                name: newSelected.name,
+                price: newSelected.price,
+                quantity: 1,
+                total: newSelected.price
+            }
 
-        cart.value.push(item)
-    } else {
-        const item = {
-            id: newSelected.id,
-            name: newSelected.name,
-            price: newSelected.price,
-            quantity: cart.value[index].quantity + 1,
-            total: (cart.value[index].quantity + 1) * newSelected.price
-        }
+            cart.value.push(item)
+        } else {
+            const item = {
+                id: newSelected.id,
+                name: newSelected.name,
+                price: newSelected.price,
+                quantity: cart.value[index].quantity + 1,
+                total: (cart.value[index].quantity + 1) * newSelected.price
+            }
 
-        cart.value[index] = item
+            cart.value[index] = item
+        }
+        
+        totalAmount.value = cart.value.reduce((acc, c) =>  acc + c.total, 0);
     }
-    
-
-    totalAmount.value = cart.value.reduce((acc, obj) =>  acc + obj.total, 0);
 })
 
-watch(
-  () => cart,
-  (newValue) => {
-    console.log(cart.value)
-    console.log(newValue.value)
-    // Note: `newValue` will be equal to `oldValue` here
-    // *unless* state.someObject has been replaced
-  },
-  { deep: true }
-)
+watch(cash, (newCash) => {
+    if (Number(newCash) > 0 && cart.value.length) {
+        cantTransact.value = totalAmount.value > newCash
+    } else {
+        cantTransact.value = true
+    }
+})
+
+
 </script>
 
 <template>
   <div id="app">
     <main class="content">
         <div class="container-fluid p-0">
-            <!-- <p>{{ selected }}</p> -->
             <div class="row">
                 <div class="col-8">
                     <div class="card">
@@ -75,14 +99,16 @@ watch(
                                         <th scope="col">Price</th>
                                         <th scope="col">Qty</th>
                                         <th scope="col">Total</th>
+                                        <th></th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr v-for="item in cart">
+                                    <tr v-for="(item, index) in cart">
                                         <th scope="row">{{ item.name }}</th>
                                         <th scope="row">{{ item.price }}</th>
-                                        <th scope="row"><input type="number" v-model="item.quantity" min="1"></th>
+                                        <th scope="row"><input type="number" class="form-control" v-model="item.quantity" @keyup="updateRowTotal(index)" min="1"></th>
                                         <th scope="row">{{ item.total }}</th>
+                                        <th scope="row"><vue-feather type="x" @click="removeFromCart(index)"></vue-feather></th>
                                     </tr>
                                 </tbody>
                             </table>
@@ -99,18 +125,18 @@ watch(
                                 </div>
 
                                 <div class="col-md-6">
-                                    <p class="fs-4">Remaining Amt: 50.00</p>
+                                    <p class="fs-4">Remaining Amt: {{ (totalAmount - cash) > 0 ? (totalAmount - cash) : 0}}</p>
                                 </div>
 
                                 <div class="col-md-12">
                                     <p class="fs-3 mt-4">CASH:</p>
-                                    <input class="form-control form-control-lg" type="text">
+                                    <input class="form-control form-control-lg" type="text" v-model="cash">
 
-                                    <p class="fs-3 mt-4">CHANGE: 10</p>
+                                    <p class="fs-3 mt-4">CHANGE: {{ (cash - totalAmount) > 0 ? Math.abs(cash - totalAmount) : 0 }}</p>
                                     
                                     <div class="d-grid gap-2">
-                                        <button class="btn btn-primary btn-lg" type="button">TRANSACT</button>
-                                        <button class="btn btn-warning btn-lg" type="button">CLEAR</button>
+                                        <button class="btn btn-primary btn-lg" type="button" :disabled="cantTransact" >TRANSACT</button>
+                                        <button class="btn btn-warning btn-lg" type="button" @click="clearAll">CLEAR</button>
                                     </div>
                                 </div>
                             </div>
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,63 +9,11 @@ import { createApp } from 'vue'
 import TurbolinksAdapter from 'vue-turbolinks'
 import App from '../app.vue'
 import VueSelect  from "vue-select";
+import VueFeather from 'vue-feather';
 
-// createApp(App).use(TurbolinksAdapter).mount('#app')
 document.addEventListener('turbo:load', () => {
   createApp(App)
     .component("v-select", VueSelect)
+    .component(VueFeather.name, VueFeather)
     .mount('#app')
 })
-
-
-// The above code uses Vue without the compiler, which means you cannot
-// use Vue to target elements in your existing html templates. You would
-// need to always use single file components.
-// To be able to target elements in your existing html/erb templates,
-// comment out the above code and uncomment the below
-// Add <%= javascript_pack_tag 'hello_vue' %> to your layout
-// Then add this markup to your html template:
-//
-// <div id='hello'>
-//   {{message}}
-//   <app></app>
-// </div>
-
-
-// import Vue from 'vue/dist/vue.esm'
-// import App from '../app.vue'
-//
-// document.addEventListener('DOMContentLoaded', () => {
-//   const app = new Vue({
-//     el: '#hello',
-//     data: {
-//       message: "Can you say hello?"
-//     },
-//     components: { App }
-//   })
-// })
-//
-//
-//
-// If the project is using turbolinks, install 'vue-turbolinks':
-//
-// yarn add vue-turbolinks
-//
-// Then uncomment the code block below:
-//
-// import Vue from 'vue'
-// import App from '../app.vue'
-
-// Vue.use(TurbolinksAdapter)
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   const app = new Vue({
-//     el: '#hello',
-//     data: () => {
-//       return {
-//         message: "Can you say hello?"
-//       }
-//     },
-//     components: { App }
-//   })
-// })
